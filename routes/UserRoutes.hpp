a2dea9ee954680a821cf2617b38a2692d29c2a65
Arayikdev <hakobjanyan.arayik2003@gmail.com>
2025-11-16 15:52:03 +0400

    work in progress

--- routes/UserRoutes.hpp
+++ routes/UserRoutes.hpp
@@ -1,22 +1,18 @@
 #include "../controllers/UserController.hpp"
-#include "../middleware/AuthMiddleware.hpp"
 void registerUserRoutes(httplib::Server &server, UserController &usercontroller)
 {
   server.Get("/users", [&](const httplib::Request &req, httplib::Response &res)
              {
-              if (!AuthMiddleware::check(req, res)) return;  
               res.set_header("Access-Control-Allow-Origin", "*"); 
               usercontroller.getUsers(req, res); });
 
   server.Get(R"(/users/([a-fA-F0-9]{24}))", [&](const httplib::Request &req, httplib::Response &res)
              {
-              if (!AuthMiddleware::check(req, res)) return;
       res.set_header("Access-Control-Allow-Origin", "*");    // Converting sub_match to std::string 
     usercontroller.getMe(req, res); });
 
   server.Put(R"(/users/([a-fA-F0-9]{24}))", [&](const httplib::Request &req, httplib::Response &res)
              {
-              if (!AuthMiddleware::check(req, res)) return;
               res.set_header("Access-Control-Allow-Origin", "*");
     usercontroller.updateProfile(req, res); });
 }
